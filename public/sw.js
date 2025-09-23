45bafa6c861dfb8f594d187921c1c4ac28e66c85
Damian Catanzaro <damiancatanzaro@gmail.com>
2025-09-23 17:43:33 -0300

    devices

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "home-dashboard-v8";
-const STATIC_CACHE = "static-v8";
-const DYNAMIC_CACHE = "dynamic-v8";
+const CACHE_NAME = "home-dashboard-v9";
+const STATIC_CACHE = "static-v9";
+const DYNAMIC_CACHE = "dynamic-v9";
 
 const urlsToCache = [
     "/",
